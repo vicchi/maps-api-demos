8342087ea0bdba44ea54dd1022079e7d4a31682b
vicchi <gary@vicchi.org>
2011-07-14 14:48:02 +0100

    Added link to GitHub repository

--- functions.php
+++ functions.php
@@ -30,11 +30,11 @@ function doHeader($demo_prefix, $use_mxn=FALSE) {
 function doFooter($use_mxn=FALSE) {
 	print "<div id=\"ft\" role=\"contentinfo\">\n";
 	if ($use_mxn) {
-		print "<p>Built with the <a href=\"http://mapstraction.com/\">Mapstraction</a> and <a href=\"http://api.maps.ovi.com/\">Ovi Maps</a> (<span id=\"build\"></span>) APIs and the <a href=\"http://developer.yahoo.com/yui/\">YUI<a> and <a href=\"http://code.google.com/p/google-code-prettify/\">Prettify</a> JavaScript libraries.</p>\n";
+		print "<p>Built with the <a href=\"http://mapstraction.com/\">Mapstraction</a> and <a href=\"http://api.maps.ovi.com/\">Ovi Maps</a> (<span id=\"build\"></span>) APIs and the <a href=\"http://developer.yahoo.com/yui/\">YUI<a> and <a href=\"http://code.google.com/p/google-code-prettify/\">Prettify</a> JavaScript libraries. Be social; the code is over on <a href=\"http://github.com/\">GitHub</a> at <a href=\"https://github.com/vicchi/maps-api-demos\">https://github.com/vicchi/maps-api-demos</a></p>\n";
 	}
 	
 	else {
-		print "<p>Built with the <a href=\"http://api.maps.ovi.com/\">Ovi Maps API</a> (<span id=\"build\"></span>) and the <a href=\"http://developer.yahoo.com/yui/\">YUI<a> and <a href=\"http://code.google.com/p/google-code-prettify/\">Prettify</a> JavaScript libraries.</p>\n";
+		print "<p>Built with the <a href=\"http://api.maps.ovi.com/\">Ovi Maps API</a> (<span id=\"build\"></span>) and the <a href=\"http://developer.yahoo.com/yui/\">YUI<a> and <a href=\"http://code.google.com/p/google-code-prettify/\">Prettify</a> JavaScript libraries. Be social; the code is over on <a href=\"http://github.com/\">GitHub</a> at <a href=\"https://github.com/vicchi/maps-api-demos\">https://github.com/vicchi/maps-api-demos</a></p>\n";
 	}
 	print "<p><a rel=\"license\" href=\"http://creativecommons.org/licenses/by-sa/2.0/uk/\"><img alt=\"Creative Commons License\" style=\"border-width:0\" src=\"http://i.creativecommons.org/l/by-sa/2.0/uk/80x15.png\" /></a>&nbsp;<span xmlns:dc=\"http://purl.org/dc/elements/1.1/\" href=\"http://purl.org/dc/dcmitype/Text\" property=\"dc:title\" rel=\"dc:type\">maps.vicchi.org</span> by <a xmlns:cc=\"http://creativecommons.org/ns#\" href=\"http://www.garygale.com/\" property=\"cc:attributionName\" rel=\"cc:attributionURL\">Gary Gale</a> is licensed under a <a rel=\"license\" href=\"http://creativecommons.org/licenses/by-sa/2.0/uk/\">Creative Commons Attribution-Share Alike 2.0 UK: England &amp; Wales License</a>.</p>\n";
 	print "</div>\n";
